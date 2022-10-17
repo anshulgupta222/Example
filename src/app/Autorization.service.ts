33f09ea9c5fb27aff5f7371678e42b2bbd3ab9fd
Anshul Gupta <anshulgupta222@gmail.com>
2022-10-18 00:02:11 +0530

    implement toastr Service

--- src/app/Autorization.service.ts
+++ src/app/Autorization.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { of } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  constructor(private toastr : ToastrService){}
 login(credential: { email: string | null ; password: string|null}): void {
     console.log('From Service', credential);
+    localStorage.setItem('credential',JSON.stringify(credential));
+    let userDetails:any = localStorage.getItem('credential');
+    console.log(JSON.parse(userDetails).email);
+    if (JSON.parse(userDetails).email === 'anshulguptaa222@gmail.com'){
+      this.toastr.success("Access Granted");
+    }
+    else {
+      this.toastr.error("Access is denied");
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { PractiseComponent } from './practise/practise.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordCheckComponent } from './password-check/password-check.component';
 import { RegistrationComponent } from './registration/registration.component';
+
 
 
 @NgModule({
@@ -21,10 +23,15 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
